73ba0ff7c333d92c7463936a1c0e7b60392d3321
Renata Inacio <revazgauska@gmail.com>
2023-04-27 14:39:31 -0300

    conceitos

---
+++ apresentacao.js
@@ -0,0 +1,43 @@
+/*
+Brendan Eich fundou JavaScript em 1995, quando trabalhava na empresa Netscape,
+que era a empresa criadora no navegador mais popular do do início dos anos 90.
+Inicialmente a linguagem foi desenvolvida com o nome Mocha, depois lançcada como 
+LiveScript, e alguns meses depois rebatizada JavaScript, aproveitando o sucesso do Java,
+que era a linguagem do momento. 
+
+A Microsoft, vendo tudo isso que estava acontecendo com o JavaScript, 
+percebeu que era uma boa ideia e resolveu seguir um caminho paralelo 
+ao JavaScript e criou o JScript para rodar somente no Internet Explorer.
+
+No ano de 1996, com o objetivo de padronizar a linguagem recém criada,
+a Netscape decidiu juntar o JavaScript à ECMA International (Associação 
+Europeia dos Fabricantes de Computadores), que é uma associação que realiza
+a especificação ou padronização de sistemas da informação.
+
+Com essa junção, foi criado um novo padrão de linguagem, que foi batizado com
+a união das palavras “ECMA” e “JavaScript”, resultando no nome “ECMAScript”.
+ECMAScript é o nome oficial da tecnologia. Entretanto, o nome JavaScript acabou
+se popularizando na comunidade dev, e até hoje a linguagem é mais conhecida por esse nome.
+Hoje, ECMAScript é utilizado mais para falar das especificações e JavaScript para se referir
+à linguagem.
+*/
+
+
+/*
+ES6 - Versão lançada em 2015, trouxe diversas novidades para a linguagem, como Default Parameters,
+Template Strings, Destructuring, entre outros
+*/
+
+/* Default Parameters */
+
+/* Template Strings */
+
+/* Destructuring */
+
+/* Strict Mode */
+
+/* Hoisting */
+
+/* var, let e const */
+
+/* for, forEach, for..of */
